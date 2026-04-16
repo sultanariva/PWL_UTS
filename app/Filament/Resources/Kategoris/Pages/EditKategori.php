714a54f3d2436e4a1bf829eda5d323485a2eb1d5
Sultan Nashira Ariva <sultannanda55@gmail.com>
2026-04-16 23:33:03 +0700

    feat(kategori): add Kategori resource with CRUD pages and form schema

---
+++ app/Filament/Resources/Kategoris/Pages/EditKategori.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Resources\Kategoris\Pages;
+
+use App\Filament\Resources\Kategoris\KategoriResource;
+use Filament\Actions\DeleteAction;
+use Filament\Resources\Pages\EditRecord;
+use Filament\Notifications\Notification;
+
+class EditKategori extends EditRecord
+{
+    protected static string $resource = KategoriResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            DeleteAction::make(),
+        ];
+    }
+}
