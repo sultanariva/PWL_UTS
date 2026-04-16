cac932d063ae6a57a282b8d7de8bd1f0776c774c
Sultan Nashira Ariva <sultannanda55@gmail.com>
2026-04-16 19:53:23 +0700

    feat(model): create Penjualan model with relationships and fillable attributes

---
+++ app/Models/Penjualan.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Penjualan extends Model
+{
+    protected $table = 't_penjualan';
+    protected $primaryKey = 'penjualan_id';
+
+    protected $fillable = [
+        'user_id',
+        'pembeli',
+        'penjualan_kode',
+        'penjualan_tanggal',
+    ];
+
+    protected $casts = [
+        'penjualan_tanggal' => 'datetime',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'user_id');
+    }
+
+    public function penjualanDetails(): HasMany
+    {
+        return $this->hasMany(PenjualanDetail::class, 'penjualan_id', 'penjualan_id');
+    }
+}
